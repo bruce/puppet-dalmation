5303300d9508f5d1cae6edc464174e2597941fc1
Bruce Williams <brwcodes@gmail.com>
2014-01-19 03:40:11 -0800

    Handle ordering, .gitignore creation

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,23 +10,32 @@ define dalmation::dotfiles($source = $title, $dir = undef) {
     source => $source
   }
 
-  file { "${repo_dir}/.install.rb":
-    mode    => "0755",
-    source  => "puppet:///modules/dalmation/install.rb",
-    require => Repository[$repo_dir],
+  ->
+
+  file { "${repo_dir}/.gitignore":
+    ensure => present
   }
 
-  exec { "install dalmation ${repo_dir}":
-    cwd     => $repo_dir,
-    command => "${repo_dir}/.install.rb",
-    require => File["${repo_dir}/.install.rb"]
-  }
+  ->
 
   file_line { "gitignore ${repo_dir} .install.rb":
     path  => "${repo_dir}/.gitignore",
     line  => '/.install.rb',
-    match => '/.install.rb.*',
-    require => Repository[$repo_dir]
+    match => '/.install.rb.*'
+  }
+
+  ->
+
+  file { "${repo_dir}/.install.rb":
+    mode    => "0755",
+    source  => "puppet:///modules/dalmation/install.rb"
+  }
+
+  ->
+
+  exec { "install dalmation ${repo_dir}":
+    cwd     => $repo_dir,
+    command => "${repo_dir}/.install.rb"
   }
 
 }
